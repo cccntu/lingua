8b47308e834d6652ff3ab9f204f245a8912fadb3
Jonathan Chang <31893406+cccntu@users.noreply.github.com>
2025-01-05 15:03:45 +0800

    Revert "compile?"
    This reverts commit 98be391fd68c1ee0e0dcf8ca6a4e4961b294bfe0.

--- lingua/transformer.py
+++ lingua/transformer.py
@@ -637,7 +637,6 @@ class SimpleMLA(nn.Module):
             bias=False,
         )
 
-    @torch.compile(mode='max-autotune-no-cudagraphs')
     def forward(
         self,
         x: torch.Tensor,
